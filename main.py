7eba7cbffed3e9c924a15b5091cddbceb67d0da9
Adrian Liechti <adrian@monobox.ch>
2024-12-24 22:28:20 +0100

    use sbert

--- main.py
+++ main.py
@@ -2,27 +2,16 @@ import os
 import io
 import base64
 import uvicorn
-import torch
 
 from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
 from typing import List, Optional
 
 from PIL import Image
-from transformers import AutoModel
+from sentence_transformers import SentenceTransformer
 
-model = AutoModel.from_pretrained(
-    os.getenv("MODEL", "jinaai/jina-embeddings-v3"),
-    trust_remote_code=True,
-)
-
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-model = model.to(device)
-
-model.eval()
-
-encode_text = getattr(model, "encode_text", getattr(model, "encode", None))
-encode_image = getattr(model, "encode_image", None)
+model_name = os.getenv("MODEL", "jinaai/jina-embeddings-v3")
+model = SentenceTransformer(model_name, trust_remote_code=True)
 
 app = FastAPI(
     title="LLM Platform Embeddings"
@@ -63,10 +52,7 @@ def embed(request: EmbeddingRequest):
     
     for i, input in enumerate(input):
         if input.text:
-            if encode_text is None:
-                raise ValueError('Model does not have a method to encode text')
-            
-            embedding = encode_text(input.text)
+            embedding = model.encode(input.text, normalize_embeddings=True)
 
             data.append({
                 "object": "embedding",
@@ -75,28 +61,25 @@ def embed(request: EmbeddingRequest):
             })
             
         if input.image:
-            if encode_image == None:
-                raise ValueError('Model does not have a method to encode image')
-            
             if input.image.startswith("http://") or input.image.startswith("https://"):
-                embedding = encode_image(input.image)
+                embedding = model.encode(input.image, normalize_embeddings=True)
             else:
                 image_data = base64.b64decode(input.image)
 
                 image = Image.open(io.BytesIO(image_data))
                 image = image.convert("RGB")
 
-                embedding = encode_image(image)
+                embedding = model.encode(image, normalize_embeddings=True)
             
             data.append({
                 "object": "embedding",
                 "index": i,
                 "embedding": embedding.tolist()
             })
-    
+
     return {
         "object": "list",
-        "model": model.name_or_path,
+        "model": model_name,
         "data": data
     }
 
